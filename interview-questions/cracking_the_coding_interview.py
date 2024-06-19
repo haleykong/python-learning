a8c3d5851b4787c5cd7755c40e014896ba8219e6
Haley Kong <haley.kong15@gmail.com>
2024-06-19 14:55:03 -0700

    Add array/string questions to cracking the coding interview. Add additional notes to python projects.

--- interview-questions/cracking_the_coding_interview.py
+++ interview-questions/cracking_the_coding_interview.py
@@ -1,10 +1,12 @@
-# 1.1 Is Unique - Determine if a string has all unique characters
+###############################################################################
+# 1.1 IS UNIQUE - Determine if a string has all unique characters
 #
 # Check - Is this an ASCII string or a Unicode string?
 
 def is_unique(str):
     # Assume the character set is ASCII
-    if len(str) > 128: return False
+    if len(str) > 128:
+        return False
 
     frequency = {}
     for char in str:
@@ -17,12 +19,13 @@ def is_unique(str):
 # Time Complexity: O(n) or O(c) where c is the size of the character set
 # Space Complexity: O(1)
 
-
-# 1.2 Check Permutation - Given two strings, write a method to decide if one
+###############################################################################
+# 1.2 CHECK PERMUTATION - Given two strings, write a method to decide if one
 # is a permutation of the other
 #
 # Check - Is the permutation comparison case-sensitive? Is whitespace
 # significant? What is the size of the character set?
+
 
 def check_permutation(string1, string2):
     if len(string1) != len(string2):
@@ -46,7 +49,8 @@ def check_permutation(string1, string2):
     return frequency1 == frequency2
 
 
-# 1.3 URLify - Write a method to replace all spaces in a string with "%20"
+###############################################################################
+# 1.3 URLIFY - Write a method to replace all spaces in a string with "%20"
 
 def urlify(str):
     url = ""
@@ -58,7 +62,8 @@ def urlify(str):
     return url
 
 
-# 1.4 Palindrome Permutation - Given a string, write a function to check if it
+###############################################################################
+# 1.4 PALINDROME PERMUTATION - Given a string, write a function to check if it
 # is a permutation of a palindrome
 
 def palindrome_permutation(input_string):
@@ -92,7 +97,158 @@ def check_max_one_odd(frequency):
 # Time Complexity: O(N) where N is the length of thes string
 
 
-# 1.5 One Away - There are three types of edits that can be performed on
-strings: insert a character, remove a 
+###############################################################################
+# 1.5 ONE AWAY - There are three types of edits that can be performed on
+# strings: insert a character, remove a character, or replace a character.
+# Check if two strings are one or zero edits away.
+
+def one_away(string1, string2):
+    if string1 == string2:
+        return True
+    # Check for replace character
+    elif len(string1) == len(string2):
+        return one_edit_replace(string1, string2)
+    # Check for insertion or removal
+    elif len(string1) + 1 == len(string2):
+        return one_edit_insert(string1, string2)
+    elif len(string1) - 1 == len(string2):
+        return one_edit_insert(string2, string1)
+    return False
+
+
+# Check if there is only one character that has been replaced
+def one_edit_replace(string1, string2):
+    found_difference = False
+    for i in range(len(string1)):
+        if string1[i] != string2[i]:
+            if found_difference:
+                return False
+            else:
+                found_difference = True
+    return True
+
+
+# Check if you can insert a character into string1 to make string2
+def one_edit_insert(string1, string2):
+    index1 = 0
+    index2 = 0
+    while (index2 < len(string2)) and (index1 < len(string1)):
+        if string1[index1] != string2[index2]:
+            if index1 != index2:
+                return False
+            index2 += 1
+        else:
+            index1 += 1
+            index2 += 1
+    return True
+
+# Time Complexity: O(n) where n is the length of the shorter string
+
+###############################################################################
+# 1.6 STRING COMPRESSION - Implement a method to perform basic string
+# compression using the counts of repeated characters. For example, the string
+# aabcccccaaa will become a2b1c5a3. If the "compressed" string would not become
+# smaller than the original string, your method should return the original
+# string. Assume only uppercase and lowercase letters.
+
+
+def string_compression(input_string):
+    compressed_str = ""
+    consecutive_count = 0
+
+    for i in range(len(input_string)):
+        consecutive_count += 1
+
+    # If end of string or if next character is different from current, append
+    # the character and count to the compressed string
+        if ((i + 1) >= len(input_string)) or (input_string[i] !=
+                                              input_string[i + 1]):
+            compressed_str += "" + input_string[i] + str(consecutive_count)
+            consecutive_count = 0
+
+    if len(compressed_str) < len(input_string):
+        return compressed_str
+    else:
+        return input_string
+
+# Time Complexity: O(p + k**2) where p is the size of the original string and
+# k is the number of consecutive character sequences
+
+###############################################################################
+# 1.7 ROTATE MATRIX - Given an image represented by an N x N matrix where each
+# pixel is 4 bytes, write a method to rotate the image by 90 degrees.
+
+
+def rotate_matrix(matrix):
+    N = len(matrix)
+    rotated_matrix = [[0] * N for i in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            rotated_x = N - j - 1
+            rotated_y = i
+            rotated_matrix[rotated_x][rotated_y] = matrix[i][j]
+    return rotated_matrix
+
+# Time Complexity: O(n**2) since any algorithm must touch all N**2 elements
+
+###############################################################################
+# 1.8 ZERO MATRIX - If an element in an M x N matrix is 0, its entire row and
+# column are set to 0
+
+
+def zero_matrix(matrix):
+    M = len(matrix)
+    N = len(matrix[0])
+    zero_rows = [False] * M
+    zero_columns = [False] * N
+
+    # Store the row and column index with value 0
+    for i in range(M):
+        for j in range(N):
+            if matrix[i][j] == 0:
+                zero_rows[i] = True
+                zero_columns[j] = True
+
+    for i in range(M):
+        if zero_rows[i]:
+            nullifyRow(matrix, i)
+
+    for j in range(N):
+        if zero_columns[j]:
+            nullifyColumn(matrix, j)
+
+    return matrix
+
+
+def nullifyRow(matrix, row):
+    for j in range(len(matrix[0])):
+        matrix[row][j] = 0
+
+
+def nullifyColumn(matrix, column):
+    for i in range(len(matrix)):
+        matrix[i][column] = 0
+
+# Notes - To reduce space, you also use the first row as a replacement for the
+# row array and the first column as a replacement for the column array. There
+# would also need to be booleans to indicate whether the first row and column
+# had zeros, and these would need to be nullifed in the end if necessary.
+
+###############################################################################
+# 1.9 STRING ROTATION - Given two strings, s1 and s2, write code to check if s2
+# is a rotation of s1.
+
+
+def string_rotation(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    s1s1 = s1 + s1
+    return s2 in s1s1
+
+# Time Complexity: O(N) if we assume isSubstring runs in O(A+ B) time where
+# A is the length of s1 and B is the length of s2
+
 # TEST CODE BELOW
-print(palindrome_permutation("abza"))
+print(string_rotation("waterbottle", "wa"))
+
--- overview.py
+++ overview.py
@@ -7,6 +7,10 @@
 # - FLOAT = floating point number (a decimal number)
 # - BOOLEAN = true or false
 
+# SCOPE = the region that a variable is recognized
+# - A variable is only available from inside the region it is created
+# - A global and locally scoped versions of the variable can be created
+
 ###############################################################################
 # MULTIPLE ASSIGNMENT
 #
@@ -84,3 +88,93 @@ website[slice]
 
 # - FOR LOOP = a statement that will execute its block of code a limited amount
 # of times
+
+# - NESTED LOOP = the "inner loop" will finish all of its iterations before
+# finishing one iteration of the "outer loop"
+
+###############################################################################
+# LOOP CONTROL
+#
+# LOOP CONTROL STATEMENTS = change a loop's execution from its normal sequence
+# - break = used to terminate the loop entirely
+# - continue = skips to the next iteration of the loop
+# - pass = does nothing, acts as a placeholder
+
+###############################################################################
+# LISTS
+#
+# LIST = used to store multiple items in a single variable
+food = ["pizza", "hamburger"]
+food.append("ice cream")  # Add an element
+food.remove("hot dog")  # Remove an element
+food.pop()  # Remove last element
+food.insert(0, "cake")  # Insert at a certain index
+food.sort()  # Sort
+food.clear()  # Clear
+
+###############################################################################
+# TUPLE
+#
+# TUPLE = collection which is ordered and unchangeable
+# - Used to group together related data
+student = ("Bro", 21, "male")
+student.count("Bro")  # Count how many times the input appears
+student.index("male")  # Returns index of value
+
+###############################################################################
+# SET
+#
+# SET = collection which is unordered and index. No duplicate values
+# - Faster than a list
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup"}
+utensils.add("napkin")  # Add an element
+utensils.remove("fork")  # Remove an element
+utensils.clear()  # Remove all elements of set
+utensils.update(dishes)  # Add one set to the other
+dinner_table = utensils.union(dishes)  # Create a new set from two sets
+# Determine what is in the first set that is not in the second
+utensils.difference(dishes)
+# Determines which elements two sets have in common
+utensils.intersection(dishes)
+
+###############################################################################
+# DICTIONARIES
+#
+# DICTIONARY = a changeable, unordered collection of unique key:value pairs
+# - Fast because they use hashing, allow us to access a value quickly
+
+capitals = {'USA': 'Washington DC',
+            'India': 'New Dehli',
+            'China': 'Beijing'}
+capitals.get('Germany')  # Check if key is in dictionary
+capitals.keys()  # Returns all keys
+capitals.values()  # Returns only values
+capitals.items()  # Returns all key-value pairs
+capitals.update({'Germany': 'Berlin'})  # Add to/update the dictionary
+capitals.pop('China')  # Remove the specified key-value pair
+capitals.clear()  # Clear the dictionary
+
+###############################################################################
+# FUNCTIONS
+#
+# FUNCTION = a block of code which is executed only when it is called
+# ARGUMENT = information that is sent to a function
+# PARAMETER = information that is used in a function
+#
+# RETURN STATEMENT = functions send Python values/objects back to the caller.
+# These values/objects are known as the function's return value.
+
+###############################################################################
+# KEYWORD ARGUMENTS
+#
+# KEYWORD ARGUMENTS = arguments preceded by an identifier when we pass them to
+# a function. The order of the arguments doesn't matter, unlike positional
+# arguments, Python knows the names of the arguments that our function receives
+
+
+def hello(first, middle, last):
+    print("Hello " + first + " " + middle + " " + last)
+
+
+hello(last="Code", middle="Dude", first="Bro")
